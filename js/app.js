c8950f1c216579c6ad2a7e5cf3c0ce9cab34ade9
aleeex <i can't believe it's not windows>
2014-10-11 01:59:24 -0400

    trying to do fizzbuzz with just javascript. not working so far..

--- js/app.js
+++ js/app.js
@@ -1,40 +1,60 @@
 //grab the area where you want to display the output and set it equal to a var
 
-var thePlace = $('#the-numbers');
-//unit test
-//thePlace.append('hello');
+// var thePlace = $('#the-numbers');
+// //unit test
+// //thePlace.append('hello');
 
-//create the variable that will house the numbers
+// //create the variable that will house the numbers
+// var theNumber = 0;
+// //console.log(theNumber);
+
+// //create a function that will print out 1 to 100
+// function printNumbers() {
+// 	for(i=0; i<100; i++){
+// 		theNumber++;
+// 		//unit test
+// 		//console.log(theNumber);
+// 		//unit test
+// 		//thePlace.append(theNumber + '<br/>');
+// 		if(theNumber % 3 == 0 && theNumber % 5 == 0){
+// 			thePlace.append('<li class="space">fizz buzz</li>');
+// 		} else if(theNumber % 3 == 0) {
+// 			thePlace.append('<li class="space">fizz</li>');
+// 		} else if(theNumber % 5 == 0) {
+// 			thePlace.append('<li class="space">buzz');
+// 		} else {
+// 			thePlace.append('<li class="space">' + theNumber + "</li>");
+// 		}
+// 	}
+// }
+
+// //call the printNumbers function
+// printNumbers();
+
+
+/*Javascript only version*/
+var thePlace = document.getElementById('the-numbers');
+
 var theNumber = 0;
-//console.log(theNumber);
 
-//create a function that will print out 1 to 100
-function printNumbers() {
+function printNumber(){
+	var theLi = document.createElement('li');
+	var fizz = document.createTextNode('fizz');
+	var buzz = document.createTextNode('buzz');
+	var fizzBuzz = document.createTextNode('fizzbuzz');
+
 	for(i=0; i<100; i++){
 		theNumber++;
-		//unit test
-		//console.log(theNumber);
-		//unit test
-		//thePlace.append(theNumber + '<br/>');
 		if(theNumber % 3 == 0 && theNumber % 5 == 0){
-			thePlace.append('<li class="space">fizz buzz</li>');
+			thePlace.innerHTML = theLi.appendChild(fizzBuzz);
 		} else if(theNumber % 3 == 0) {
-			thePlace.append('<li class="space">fizz</li>');
+			thePlace.innerHTML = theLi.appendChild(fizz);
 		} else if(theNumber % 5 == 0) {
-			thePlace.append('<li class="space">buzz');
-		} else {
-			thePlace.append('<li class="space">' + theNumber + "</li>");
-		}
+			thePlace.innerHTML = theLi.appendChild(buzz);
+		} //else {
+			//thePlace.innerHTML = theLi.appendChild(theNumber);
+		//}
 	}
 }
 
-//call the printNumbers function
-printNumbers();
-
-/*
-	Description:
-	Returns a reference to the element by its ID.
-
-	Syntax:
-	element = document.getElementById(id);
-*/
+printNumber();
