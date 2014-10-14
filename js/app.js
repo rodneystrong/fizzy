b8dc0af04e9dd97a46121e0201eac0cd0b4835ec
aleeex <i can't believe it's not windows>
2014-10-14 04:01:39 -0400

    fixed the loop and created correct code to wait for DOM load before displaying fizzbuzz

--- js/app.js
+++ js/app.js
@@ -35,29 +35,39 @@
 /*Javascript only version*/
 var thePlace = document.getElementById('the-numbers');
 
+var theButton = document.getElementById('theButton');
+
 function printNumber(){
-	var theLi = document.createElement('li');
 
 	var fizz = document.createTextNode('fizz');
 	var buzz = document.createTextNode('buzz');
 	var fizzBuzz = document.createTextNode('fizzbuzz');
 
-	var theFizz = theLi.appendChild(fizz);
-	var theBuzz = theLi.appendChild(buzz);
-	var theFizzBuzz = theLi.appendChild(fizzBuzz);
+	//removing these as unnecessary for now
+	// var theFizz = theLi.appendChild(fizz);
+	// var theBuzz = theLi.appendChild(buzz);
+	// var theFizzBuzz = theLi.appendChild(fizzBuzz);
 
-	for(i=0; i<100; i++){
-		if(theNumber % 3 == 0 && theNumber % 5 == 0){
-			thePlace.innerHTML = "<li>fizzbuzz</li>";
-		} else if(theNumber % 3 == 0) {
-			thePlace.innerHTML = "<li>fizz</li>";
-		} else if(theNumber % 5 == 0) {
-			thePlace.innerHTML = "<li>buzz</li>";
+	for(i=1; i<100; i++){
+		var theLi = document.createElement('li');
+		if((i % 3 == 0) && (i % 5 == 0)){
+			theLi.innerHTML = "fizzBuzz";
+		} else if(i % 3 == 0) {
+			theLi.innerHTML = "fizz";
+		} else if(i % 5 == 0) {
+			theLi.innerHTML = "buzz";
 		} 
-		// else {
-		// 	thePlace.appendChild(theNumber);
-		// }
+		else {
+			theLi.innerHTML = i;
+		}
+
+		thePlace.appendChild(theLi);
+		var attr = document.createAttribute('class');
+		attr.value = 'space';
+		theLi.setAttributeNode(attr);
 	}
 }
 
-printNumber();
+//printNumber();
+
+//theButton.onclick = printNumber();
